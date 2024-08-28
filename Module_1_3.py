f48a53a9fc7a70e2e3a0eb34bc1902641b964ae7
Karshik-user <kurasov86@mail.ru>
2024-08-28 21:19:38 +0400

    Д/З от 28.08.2024г

---
+++ Module_1_3.py
@@ -0,0 +1,9 @@
+#Практическая работа по уроку "Динамическая типизация"
+
+name = "Сергей"
+print('Name:',(name))
+age = 37
+print("Age:",(age))
+print("New Age:",(age + 1))
+is_student = True
+print("Is Student:",(is_student))
